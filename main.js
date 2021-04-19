4e751df36c0aff48c5daeb5b54eccd3246d59790
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-04-19 21:36:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-//"https://teachablemachine.withgoogle.com/models/S5xbpuDaJ"//
+function setup() 
+{
+    canvas = createCanvas(300, 240);
+    canvas.position(580, 200);
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/S5xbpuDaJ/model.json',modelLoaded);
+}
+
+function modelLoaded()
+{
+console.log('Model Loaded!');
+}
+
+function draw()
+{
+image(video, 0, 0, 300, 300);
+classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results){
+if (error) {
+console.error(error);
+} else {
+console.log(results);
+document.getElementById("result_object_family_member").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+}
